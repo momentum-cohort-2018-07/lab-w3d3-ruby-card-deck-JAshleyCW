bfd9fff93e33fd53b87628c5124848abbf0efed1
JAshleyCW <ashley.whitted@bcbsnc.com>
2018-08-17 10:40:39 -0400

    card is done

--- deck_starter_files/lib/deck.rb
+++ deck_starter_files/lib/deck.rb
@@ -1,4 +1,14 @@
 require_relative "card"
 
 class Deck
+
+    def initialize
+        #needs to 52 cards
+        @cards = []
+        #need 52 to Cards (from card class) to an array
+        52.times{@cards.push(Card.new(:A, :spades))}
+    end
+
+
+
 end
